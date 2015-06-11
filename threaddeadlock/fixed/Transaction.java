9bc7f72219e5d3e20883592c3d6a7020838ea192
Gobinath <slgobinath@gmail.com>
2015-06-11 11:24:23 +0530

    Thread deadlock

--- threaddeadlock/fixed/Transaction.java
+++ threaddeadlock/fixed/Transaction.java
@@ -1,29 +1,31 @@
 public class Transaction extends Thread {
-	private final String id;
-	private final Account from;
-	private final Account to;
-	private final double amount;
+    private final String id;
+    private final Account from;
+    private final Account to;
+    private final double amount;
 
-	public Transaction(String id, Account from, Account to, double amount) {
-		this.id = id;
-		this.from = from;
-		this.to = to;
-		this.amount = amount;
-	}
+    public Transaction(String id, Account from, Account to, double amount) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+    }
 
-	@Override
-	public void run() {
-		synchronized (from) {
-			from.withdraw(amount);
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-			}
+    @Override
+    public void run() {
+        // Acquire the lock of Account 'from'
+        synchronized (from) {
+            from.withdraw(amount);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) { }
         }
-		synchronized (to) {
-			to.deposit(amount);
-		}
-		System.out.println(from + " is released by " + id);
-		System.out.println(amount + "is transfered from " + from + " to " + to);
-	}
+        // Release the lock of Account 'from'
+        // Acquire the lock of Account 'to'
+        synchronized (to) {
+            to.deposit(amount);
+        }
+        // Release the lock of Account 'to'
+        System.out.println(amount + "is transfered from " + from + " to " + to);
+    }
 }
