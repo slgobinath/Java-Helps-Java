18adf59b8c6574e354d90698fe624630ab13597a
Gobinath <slgobinath@gmail.com>
2016-05-07 17:10:02 +0530

    WSO2 Carbon web service

---
+++ wso2/carbon/helloworld/com.javahelps.samplecarbon.service/src/main/java/com/javahelps/samplecarbon/service/SampleService.java
@@ -0,0 +1,9 @@
+package com.javahelps.samplecarbon.service;
+
+public class SampleService {
+    private static final String[] LANGUAGES = { "Java", "C++", "Python", "Go" };
+
+    public String[] getLanguages() {
+        return LANGUAGES;
+    }
+}
