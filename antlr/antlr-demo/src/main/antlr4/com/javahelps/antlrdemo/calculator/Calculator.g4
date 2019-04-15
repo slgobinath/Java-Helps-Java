41eaaa90ffdbab03a329a02580c75ca67af890a9
Gobinath <slgobinath@gmail.com>
2019-04-15 07:04:31 -0400

    ANTLR Hello World Calculator

---
+++ antlr/antlr-demo/src/main/antlr4/com/javahelps/antlrdemo/calculator/Calculator.g4
@@ -0,0 +1,16 @@
+
+grammar Calculator;
+
+
+operation
+    : left=NUMBER operator='+' right=NUMBER
+    | left=NUMBER operator='-' right=NUMBER
+    | left=NUMBER operator='*' right=NUMBER
+    | left=NUMBER operator='/' right=NUMBER
+    ;
+
+NUMBER
+   : ('0' .. '9') + ('.' ('0' .. '9') +)?
+   ;
+
+WS : (' ' | '\t')+ -> channel(HIDDEN);
---
+++ antlr/antlr-demo/src/main/java/com/javahelps/antlrdemo/calculator/Calculator.java
@@ -0,0 +1,34 @@
+package com.javahelps.antlrdemo.calculator;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CodePointCharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.calculate("2 + 5"));  // 7.0
+        System.out.println(calculator.calculate("2 * 5"));  // 10.0
+        System.out.println(calculator.calculate("5 - 3"));  // 2.0
+        System.out.println(calculator.calculate("5 / 3"));  // 1.6666666666666667
+        System.out.println(calculator.calculate("5 # 3"));  // Error: line 1:2 token recognition error at: '#'
+    }
+
+    private Double calculate(String source) {
+        CodePointCharStream input = CharStreams.fromString(source);
+        return compile(input);
+    }
+
+    private Double compile(CharStream source) {
+        CalculatorLexer lexer = new CalculatorLexer(source);
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        CalculatorParser parser = new CalculatorParser(tokenStream);
+        ParseTree tree = parser.operation();
+        CalculatorVisitorImpl visitor = new CalculatorVisitorImpl();
+        return visitor.visit(tree);
+    }
+}
